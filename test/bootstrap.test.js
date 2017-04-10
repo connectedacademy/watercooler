cd81bbcd6a7e1a037f29c80a714bf9df4a9088d2
tombartindale <tom@bartindale.com>
2017-04-10 12:52:04 +0100

    test func

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,5 +1,33 @@
 var sails = require('sails');
 
+var flat = require('flat');
+var _ = require('lodash');
+
+_.deepTypeMatch = function(expected, actual)
+{
+  var exp_flatted = flat(expected);
+  var exp_mapped = _.mapValues(exp_flatted,(v,k)=>{
+    return typeof(exp_flatted[k])
+  });
+  var act_flatted = flat(actual);
+  var act_mapped = _.mapValues(act_flatted,(v,k)=>{
+    return typeof(act_flatted[k])
+  });
+
+  return _.isEqual(act_mapped,exp_mapped);
+};
+
+let bodyCheck = function(body, comparefile)
+{
+  let example = comparefile;
+  if (_.isString(comparefile))
+    example = require('../spec/examples/' + comparefile + '.json');
+  if (!_.deepTypeMatch(body,example))
+    throw new Error("Result does not match the required fields");
+}
+
+global.bodyCheck = bodyCheck;
+
 before(function(done) {
 
   // Increase the Mocha timeout so that Sails has enough time to lift.
