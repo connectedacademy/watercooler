0854c7bdf3b66e2a857eebe4b31e133c42a5aae9
tombartindale <tom@bartindale.com>
2017-04-10 13:11:31 +0100

    valid tests so far

--- test/http/endpoints.test.js
+++ test/http/endpoints.test.js
@@ -1,41 +1,20 @@
 var request = require('supertest');
 var expect = require('chai').expect;
 
-var flat = require('flat');
-var _ = require('lodash');
-
-_.deepTypeMatch = function(expected, actual)
-{
-  var exp_flatted = flat(expected);
-  var exp_mapped = _.mapValues(exp_flatted,(v,k)=>{
-    return typeof(exp_flatted[k])
-  });
-  var act_flatted = flat(actual);
-  var act_mapped = _.mapValues(act_flatted,(v,k)=>{
-    return typeof(act_flatted[k])
-  });
-
-  return _.isEqual(act_mapped,exp_mapped);
-};
-
-let bodyCheck = function(body, comparefile)
-{
-  let example = comparefile;
-  if (_.isString(comparefile))
-    example = require('../spec/examples/' + comparefile + '.json');
-  if (!_.deepTypeMatch(body,example))
-    throw new Error("Result does not match the required fields");
-}
-
-global.bodyCheck = bodyCheck;
-
 describe('Auth', function() {
   
   describe('user login', function() {
     it('should redirect to Twitter', function (done) {
       request(sails.hooks.http.app)
         .get('/auth/login')
+        .set('Referer',process.env.TEST_DOMAIN)
         .expect(302,done);
+    });
+
+    it('should error when no origin or referrer', function (done) {
+      request(sails.hooks.http.app)
+        .get('/auth/login')
+        .expect(500,done);
     });
   });
 
@@ -43,32 +22,41 @@ describe('Auth', function() {
     it('should redirect to GitHub', function (done) {
       request(sails.hooks.http.app)
         .get('/admin/login')
+        .set('Referer',process.env.TEST_DOMAIN)
         .expect(302,done);
+    });
+
+    it('should error when no origin or referrer', function (done) {
+      request(sails.hooks.http.app)
+        .get('/admin/login')
+        .expect(500,done);
     });
   });
 
   describe('user logout', function() {
-    it('should return message', function () {
-      return request(sails.hooks.http.app)
+    it('should return message', function (done) {
+      request(sails.hooks.http.app)
         .get('/auth/logout')
         .expect(200)
-        .then(response => {
-          expect(response.body).to.have.property('msg');
-        });
+        .expect((res)=>{
+          bodyCheck(res.body,{
+            msg:'msg'
+          });
+        })
+        .end(done);
     });
   });
 
   describe('admin logout', function() {
-    it('should return message', function () {
-      return request(sails.hooks.http.app)
+    it('should return message', function (done) {
+      request(sails.hooks.http.app)
         .get('/admin/logout')
-        .expect(200)
-        .then(response => {
-          deepTypeMatch({
-            msg:'string msg'
-          },
-          response.body);
-        });
+        .expect((res)=>{
+          bodyCheck(res.body,{
+            msg:'msg'
+          });
+        })
+        .expect(200,done);
     });
   });
 });
