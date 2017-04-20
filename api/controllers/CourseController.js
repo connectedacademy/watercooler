ca96420c7b42b11a96b6e3cae77d4f45ebafb49c
tombartindale <tom@bartindale.com>
2017-04-20 13:52:38 +0100

    fix for registration lang and remove url from course spec

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -2,6 +2,8 @@ let applyFrontMatter = async (data,uri)=>{
     let courseinfo = await CacheEngine.getFrontmatter(uri);
     _.extend(data, courseinfo);
     delete data.published;
+    if (data.video)
+        delete data.url;
 }
 
 module.exports = {
@@ -9,28 +11,30 @@ module.exports = {
     spec: async (req,res)=> {
         try
         {
+            let lang = await LangService.lang(req);
+
             var data = await CacheEngine.getSpec(req,res);
 
             let promises = [];
 
             //course info:
-            promises.push(applyFrontMatter(data, req.course.url + '/course/content/' + LangService.lang(req) + '/info.md'));
+            promises.push(applyFrontMatter(data, req.course.url + '/course/content/' + lang + '/info.md'));
 
             //for each file in the spec, get the markdown and parse it:
             for (let klass of data.classes)
             {
-                promises.push(applyFrontMatter(klass, req.course.url + '/course/content/' + LangService.lang(req) + '/' + klass.dir + '/info.md'));
+                promises.push(applyFrontMatter(klass, req.course.url + '/course/content/' + lang + '/' + klass.dir + '/info.md'));
                 for (let content of klass.content)
                 {
                     if (content.url)
-                        promises.push(applyFrontMatter(content, req.course.url + '/course/content/' + LangService.lang(req) + '/' + klass.dir + '/' + content.url));
+                        promises.push(applyFrontMatter(content, req.course.url + '/course/content/' + lang + '/' + klass.dir + '/' + content.url));
                 }
             }
 
             let results = await Promise.all(promises);
 
-            data.baseUri = req.course.url + '/course/content/' + LangService.lang(req) + '/';
-            data.currentLang =  LangService.lang(req);
+            data.baseUri = req.course.url + '/course/content/' + lang + '/';
+            data.currentLang = lang;
 
             return res.json(data);
         }
--- api/services/LangService.js
+++ api/services/LangService.js
@@ -1,8 +1,18 @@
 module.exports = {
-    lang: (req) =>
+    lang: async (req) =>
     {
         if (req.session.passport && req.session.passport.user)
-            return req.session.passport.user.lang;
+        {
+            if (!req.session.passport.user.registration)
+            {
+                let reg = Registration.findOne({
+                    user: req.session.passport.user.id,
+                    course: req.course.domain
+                });
+                req.session.passport.user.registration = reg;
+            }
+            return req.session.passport.user.registration.lang;
+        }
         else
             return 'en';
     }
