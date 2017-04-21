7a0b378faf8717a83041981e59709e7ebc9592f7
tombartindale <tom@bartindale.com>
2017-04-21 09:04:08 +0100

    initial sendgrid

--- api/services/NotificationEngine.js
+++ api/services/NotificationEngine.js
@@ -1,5 +1,40 @@
+let helper = require('sendgrid').mail;
+var sg = require('sendgrid')(process.env.SENDGRID_KEY);
+
+let sendEmail = function(user, subject, content){
+
+    var from_email = new helper.Email(process.env.FROM_EMAIL);
+    var to_email = new helper.Email(user.email);
+    var subject = subject;
+    var content = new helper.Content('text/plain', content);
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+    });
+
+    sg.API(request, function(error, response) {
+        if (error)
+            sails.log.error(error);
+        else
+            sails.log.verbose('Mail Sent',response);
+    });
+};
+
+//TODO: i18n email content
+//TODO: html email content (based on markdown)
+
 module.exports = {
     
+    //welcome to the course
+    signup: (req,user)=>{
+        sails.log.verbose('Sending signup notification',user);
+        let course = req.course.domain;
+        sendEmail(user,'Welcome to '+course,'Welcome to the ' + course + ' course');
+    },
+
     //TODO: notifications
 
     // course starting (1 week before)
