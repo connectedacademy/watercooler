3d073c20c1d12923ec101be38f18c4555e1741c4
tombartindale <tom@bartindale.com>
2017-11-08 13:57:04 +0000

    update docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,11 +9,15 @@ define({
     "withGenerator": false,
     "withCompare": false
   },
+  "header": {
+    "title": "Watercooler API",
+    "content": "<h1>Connected Academy</h1>\n<p>This documentation is a work in progress...</p>\n<h2>Socket Messages</h2>\n<p>All messages come back with the following:</p>\n<pre><code class=\"language-json\">{\n    &quot;msgtype&quot;: &quot;message&quot;, // message|classroom|discussion\n    &quot;msg&quot;: ...\n}\n</code></pre>\n<h3>Discussion Messages</h3>\n<p>Subcriptions are created for user related information when the following is called:</p>\n<p><code>/v1/auth/me</code></p>\n<p>Emits <code>discussion</code> messages on new peer discussion messages.</p>\n<h3>Dashboard</h3>\n<p>Subscriptions for all messages coming in during a class:</p>\n<p><code>/v1/classroom/mycode/:class</code></p>\n<p>Emits <code>classroom</code> messages on new user registrations, or <code>message</code> messages on new comments from students in your classroom.</p>\n<h3>Message Subscriptions</h3>\n<p>For a time window:</p>\n<p><code>/v1/messages/subscribe/:class/:content/:startsegment/:endsegment</code></p>\n<p>For static content (i.e. webinar):</p>\n<p><code>/v1/messages/content/:class/:content</code></p>\n"
+  },
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-11-08T13:44:09.304Z",
+    "time": "2017-11-08T13:56:30.569Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
---
+++ header.md
@@ -0,0 +1,40 @@
+# Connected Academy
+
+This documentation is a work in progress...
+
+## Socket Messages
+
+All messages come back with the following:
+
+```json
+{
+    "msgtype": "message", // message|classroom|discussion
+    "msg": ...
+}
+```
+
+### Discussion Messages
+
+Subcriptions are created for user related information when the following is called:
+
+`/v1/auth/me`
+
+Emits `discussion` messages on new peer discussion messages.
+
+### Dashboard
+
+Subscriptions for all messages coming in during a class:
+
+`/v1/classroom/mycode/:class`
+
+Emits `classroom` messages on new user registrations, or `message` messages on new comments from students in your classroom.
+
+### Message Subscriptions
+
+For a time window:
+
+`/v1/messages/subscribe/:class/:content/:startsegment/:endsegment`
+
+For static content (i.e. webinar):
+
+`/v1/messages/content/:class/:content`
