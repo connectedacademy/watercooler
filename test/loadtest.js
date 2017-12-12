8a4cb1676232348e0f00a90d0cdb007cd58d7c37
tombartindale <tom@bartindale.com>
2017-12-12 10:36:25 +0000

    loadtest update

--- test/loadtest.js
+++ test/loadtest.js
@@ -6,17 +6,22 @@ var bluebird = require('bluebird');
 
 const WINDOW_RANGE = 60;
 const START_VARIATION = 60;
-// const MAX_TIME = 1600;
-const MAX_TIME = 200;
+const MAX_TIME = 3000;
+// const MAX_TIME = 200;
 const DELAY = 5000;
 // const NUM_AGENTS = 2;
-const NUM_AGENTS = 50;
-const INIT_DELAY = 30000;
+const NUM_AGENTS = 2;
+const INIT_DELAY = 3000;
 const RANDOM_PLAYBACK = false;
+const CREATE_MESSAGES = true;
 const USER_LOGIN = true;
-const USER = 'edjenkinsca';
-const PSK = process.env.PSK;
-const URL = 'interpretation.connectedacademy.io';
+const USER = 'tombartindale';
+const PSK = '';
+const URL = 'talkingpictures.connectedacademy.io';
+const KLASS = 'interpretation';
+const CONTENT = 'liveclass';
+const INCLASSROOM = true;
+const FAKE_USER_LOGIN = true;
 
 go();
 
@@ -25,16 +30,16 @@ async function go()
     let agents = [];
     for (let i=0;i<NUM_AGENTS;i++)
     {
-        let a = request.agent('https://api.connectedacademy.io');
+        // let a = request.agent('https://api.connectedacademy.io');
+        let a = request.agent('http://localhost:4000');        
         a.min = Number.MAX_VALUE;
-        // a.max = 0;
         a.calls = [];
-        // a.total = 0;
         agents.push(a);
     }
 
     let requests = [];
     let id = 0;
+    let classcode = '';
     for (let agent of agents)
     {
         let name = id;
@@ -50,8 +55,55 @@ async function go()
             {
                 await agent.get(`/v1/auth/loginexistinguser/?account=${USER}&psk=${PSK}&callback=https%3A%2F%2F${URL}`);
             }
+
+            //teacher
+            if (INCLASSROOM && name == 0)
+            {
+                await agent.get(`/v1/auth/loginexistinguser/?account=${USER}&psk=${PSK}&callback=https%3A%2F%2F${URL}`);
+                let classcoderes = await agent.get(`/v1/classroom/mycode/${KLASS}`)
+                .set('Referer', `https://${URL}`);
+                // console.log(classcoderes);
+                classcode = classcoderes.body.code;
+                console.log(`[${name}] is teacher ${classcode}`);
+
+                await agent.post(`/v1/classroom/inclass`)
+                .send({
+                    code: classcode
+                    })
+                .set('Referer', `https://${URL}`);
+                console.log(`[${name}] teacher in class ${classcode}`);
+            }
+
+            //students
+            if (FAKE_USER_LOGIN && name > 0)
+            {
+                await agent.get(`/v1/auth/testuserlogin/?psk=${PSK}&callback=https%3A%2F%2F${URL}`);
+
+                await agent.post(`/v1/auth/register/`)
+                .send({
+                    "email":"test@connectedacademy.io",
+                    "lang":"en",
+                    "hub_id":"ncl",
+                    "registration_info":{},
+                    "consent":true
+                    })
+                .set('Referer', `https://${URL}`);
+
+                if (INCLASSROOM)
+                {
+                    // await bluebird.delay(Math.random() * 1000);
+                    await agent.post(`/v1/classroom/inclass`)
+                    .send({
+                        code: classcode
+                        })
+                    .set('Referer', `https://${URL}`);
+                    console.log(`[${name}] in class ${classcode}`);
+                }
+            }
             
+            console.log(`[${name}] init delay ${initdelay}`);            
             await bluebird.delay(initdelay);
+            console.log(`[${name}] starting`);
 
             for (let segment = start;segment<MAX_TIME;segment+=5)
             {
@@ -63,10 +115,23 @@ async function go()
                     end = start + WINDOW_RANGE;
                 }
 
+                if (CREATE_MESSAGES)
+                {
+                    // if (Math.random() > 0.1)
+                    {
+                        await agent.post(`/v1/messages/create`)
+                        .send({
+                            text:`#test loadtest message from ${name} at ${new Date()} https://${URL}/#/course/${KLASS}/${CONTENT}/${start}`
+                        })
+                        .set('Referer', `https://${URL}`);
+                        console.log(`[${name}] created message at ${start}`);
+                    }
+                }
+
                 console.log(`[${name}] calling ${start} - ${end}`);
 
                 let stime = new Date();
-                await agent.get(`/v1/messages/summarybatch/interpretation/liveclass/${start}/${end}/5?whitelist=false`)
+                await agent.get(`/v1/messages/summarybatch/${KLASS}/${CONTENT}/${start}/${end}/5?whitelist=false`)
                 .set('Referer',`https://${URL}`)
                 .expect(200);
 
