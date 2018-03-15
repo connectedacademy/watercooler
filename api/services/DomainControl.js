6af041b03423269bcacb236f5390a79b510a2c2f
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-03-15 16:10:41 +0000

    Updated domain control

--- api/services/DomainControl.js
+++ api/services/DomainControl.js
@@ -1,35 +1,44 @@
-let master_config = process.env.MASTER_REPO + 'config/courses.yml';
-let URL = require('url');
+let master_config = process.env.MASTER_REPO + 'config/courses.yml'
+let URL = require('url')
 
 module.exports = {
 
-    verifyDomain: async (req)=>{
-        //parse domain
-        let url = URL.parse(req.get('referer') || req.get('origin'));
-        let domain = url.hostname;
-        
-        let whitelist = await DomainControl.getWhitelist();
+  verifyDomain: async (req) => {
+    
+    // Parse domain
+    let url = URL.parse(req.get('referer') || req.get('origin'))
+    
+    let domain = url.hostname
 
-        let isvalid = false
-        _.each(whitelist.courses,(entry)=>{
-            //if domain is in whitelist, continue, if not, fail 
-            if (entry.domain === domain)
-                isvalid = entry;
-        });
+    let whitelist = await DomainControl.getWhitelist()
 
-        if (domain == 'localhost' && process.env.DEBUG_MODE=='true')
-        {
-            return whitelist.courses[_.first(Object.keys(whitelist.courses))];
-        }
-        else
-        {
-            return isvalid;
-        }
-    },
+    sails.log.verbose('whitelist:')
+    sails.log.verbose(whitelist)
 
-    getWhitelist: async ()=>{
-        let whitelist = await CacheEngine.getYaml('connectedacademy.io', master_config);
-        return whitelist;
+    let isvalid = false
+
+    _.each(whitelist.courses,(entry) => {
+      // if domain is in whitelist, continue, if not, fail
+      if (entry.domain === domain) {
+        isvalid = entry
+      }
+    })
+
+    sails.log.verbose('isvalid:')
+    sails.log.verbose(isvalid)
+
+    if (domain == 'localhost' && process.env.DEBUG_MODE == 'true') {
+      sails.log.verbose('Returning first course in whitelist')
+      return whitelist.courses[_.first(Object.keys(whitelist.courses))]
+    } else {
+      sails.log.verbose('Returning found course:')
+      sails.log.verbose(isvalid)
+      return isvalid
     }
+  },
 
+  getWhitelist: async () => {
+    let whitelist = await CacheEngine.getYaml('connectedacademy.io', master_config)
+    return whitelist
+  }
 }
